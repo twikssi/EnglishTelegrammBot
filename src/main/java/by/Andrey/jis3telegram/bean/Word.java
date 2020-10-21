e2e8b5859f6937a3cf0ae4d07e496cae193fdbfb
twikssi <limpbiski15@gmail.com>
2020-10-21 13:27:44 +0300

    created enum part of speech and java bean Word

--- src/main/java/by/Andrey/jis3telegram/bean/Word.java
+++ src/main/java/by/Andrey/jis3telegram/bean/Word.java
@@ -1,4 +1,14 @@
 package by.Andrey.jis3telegram.bean;
 
+import by.Andrey.jis3telegram.enums.PartsOfSpeech;
+import lombok.Data;
+
+@Data
 public class Word {
+    private String name;
+    private PartsOfSpeech partsOfSpeech;
+    private String transcription;
+    private String meaning;
+    private long numberOfRepetitions = 0;
+    private boolean isDaily = false;
 }
