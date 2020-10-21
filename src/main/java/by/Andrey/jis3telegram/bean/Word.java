1013496b86be9a479bb4318dac709cc6ef07dcb7
twikssi <limpbiski15@gmail.com>
2020-10-21 13:42:52 +0300

    created allargs, my and no args  constructors in class word

--- src/main/java/by/Andrey/jis3telegram/bean/Word.java
+++ src/main/java/by/Andrey/jis3telegram/bean/Word.java
@@ -1,9 +1,13 @@
 package by.Andrey.jis3telegram.bean;
 
 import by.Andrey.jis3telegram.enums.PartsOfSpeech;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Word {
     private String name;
     private PartsOfSpeech partsOfSpeech;
@@ -12,4 +16,13 @@ public class Word {
     private String example;
     private long numberOfRepetitions = 0;
     private boolean isDaily = false;
+
+    public Word(String name, PartsOfSpeech partsOfSpeech, String transcription, String meaning, String example, boolean isDaily) {
+        this.name = name;
+        this.partsOfSpeech = partsOfSpeech;
+        this.transcription = transcription;
+        this.meaning = meaning;
+        this.example = example;
+        this.isDaily = isDaily;
+    }
 }
